c3c4a4654b89e1f9a8a1679a45e3af2f8d031aa8
AMuhammadFarhanHakzah <amf21022002@gmail.com>
2024-03-13 06:12:48 +0800

    auth, migration

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -49,14 +49,14 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="{{url('/')}}">Home</a></li>
+          <li><a class="nav-link scrollto active" href="{{url('/')}}#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="{{url('/')}}#about">About</a></li>
           <li><a class="nav-link scrollto" href="{{url('/')}}#services">Services</a></li>
           <li><a class="nav-link scrollto" href="{{url('/')}}#team">Team</a></li>
           <li><a href="{{url('/berita')}}">Berita</a></li>
           <li><a href="{{url('/pengumuman')}}">Pengumuman</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Login</a></li>
+          <li><a class="getstarted scrollto" href="{{'login'}}">Login</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
